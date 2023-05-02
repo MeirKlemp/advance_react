cf89834b76fb6e07de5a37757a85b00f418f4660
chananelz <chananelz@gmail.com>
2023-05-02 12:49:23 +0300

    end work 12:50

--- albums/src/pages/postDetail.jsx
+++ albums/src/pages/postDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 export default function PostDetail() {
+  // TODO: posted detail and comments go here https://scrimba.com/learn/reactrouter6/route-params-part-32-useparams-challenge-co78c4ed9bed416eaf11fdb44
   return <h1> PostDetail page goes here</h1>;
 }
