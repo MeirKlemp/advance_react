d126ec135a630cbc17429a17d3775399157a14af
chananelz <chananelz@gmail.com>
2023-05-07 12:22:01 +0300

    start work on post details

--- albums/src/pages/posts.jsx
+++ albums/src/pages/posts.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { useResource } from "../api";
+import { UserContext } from "../App";
 
 export default function Posts() {
-  const [posts, setPosts] = useResource("posts");
+  const { user } = useContext(UserContext);
+  const [posts, setPosts] = useResource("posts?userId=" + user.id);
+  console.log(`posts: ${JSON.stringify(posts)}`);
+
+  const postEls = posts?.map((post) => (
+    <Link
+      to={`/posts/${post.id}`}
+      key={post.id}
+      className="host-van-link-wrapper"
+    >
+      <div className="host-van-single" key={post.id}>
+        <div className="host-van-info">
+          <h3>{post.title}</h3>
+          <p>{post.body.substring(0, 15)}...</p>
+        </div>
+      </div>
+    </Link>
+  ));
+
   return (
-    <div>
-      <h1>Posts</h1>
-      {posts &&
-        posts.map((post) => (
-          <Link key={post.id} to={`/posts/${post.id}`}>
-            <h2>{post.title}</h2>
-          </Link>
-        ))}
-    </div>
+    <section>
+      <h1 className="host-vans-title">Your posts</h1>
+      <div className="host-vans-list">
+        {posts !== null ? <section>{postEls}</section> : <h2>Loading...</h2>}
+      </div>
+    </section>
   );
 }
