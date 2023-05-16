38c986ddcd1fc0676765af8e808646e764075810
Meir Klemfner <meirk251@outlook.com>
2023-05-16 21:12:49 +0300

    Finish home page

--- albums/src/pages/home.jsx
+++ albums/src/pages/home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import WelcomeImage from "../assets/images/welcome.png";
 
 export default function Home() {
   return (
     <div className="container">
       <h1>Welcome</h1>
-      <img src="../assets/images/welcome.png" alt="social media" />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src={WelcomeImage} alt="social media" />
+      </div>
     </div>
   );
 }
